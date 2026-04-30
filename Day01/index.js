3e3ffbfd6fa4ebd5010d4568ca47540b4153925c
Arun N <tgnarun@gmail.com>
2026-04-30 14:33:38 +0530

    Day 1 exercises: string, mathematical, and logical operations

---
+++ Day01/index.js
@@ -0,0 +1,33 @@
+let a = 24;
+let b = 12;
+
+let str = "Hello, World!";
+
+// String operations :
+console.log("String operations :")
+
+console.log("String:", str);
+console.log("Length of string:", str.length);
+console.log("Uppercase:", str.toUpperCase());
+console.log("Lowercase:", str.toLowerCase());
+console.log("Substring (0-5):", str.substring(0, 5)); 
+
+// Mathematical operations :
+console.log("Mathematical operations :")
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Remainder:", a % b); 
+
+
+// Logical operations :
+console.log("Logical operations :")
+
+console.log("a > b:", a > b); // T
+console.log("a < b:", a < b); // F
+console.log("a == b:", a == b); // F 
+console.log("a != b:", a != b); // T
+console.log("a >= b:", a >= b); // T
+console.log("a <= b:", a <= b); // F
