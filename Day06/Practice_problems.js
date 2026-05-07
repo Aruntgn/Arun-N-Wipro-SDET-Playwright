b3c3dd7f1206a32fbdc86e8860bdb26180ea2a37
Aruntgn <tgnarun@gmail.com>
2026-05-07 09:51:05 +0530

    Practice problems solutions

--- Day06/Practice_problems.js
+++ Day06/Practice_problems.js
@@ -56,6 +56,89 @@ function cleanFlights(flights) {
     // S4: Return JSON string
     return JSON.stringify(filteredFlights, null, 2);
 }
-
+console.log("Q1: FLIGHT BOOKING DATA CLEANER RESULT\n");
 console.log("******CLEANED FLIGHT DATA******");
 console.log(cleanFlights(rawFlights));
+
+
+
+// Q2: E-COMMERCE DISCOUNT APPLICATOR
+
+/*
+Approach:
+1. Create a callback function to identify eligible products.
+2. Check whether the product belongs to Electronics category
+   and has price greater than $200.
+3. Use map() to transform the cart items.
+4. Apply 10% discount for eligible products.
+5. Add isDiscounted property to all products.
+6. Track total discounted items and savings amount.
+7. Use template literals for summary output.
+8. Use setTimeout() to simulate delayed server response.
+*/
+
+const cart = [
+    { name: "Gaming Laptop", price: 1200, category: "Electronics" },
+    { name: "Coffee Maker", price: 85, category: "Kitchen" },
+    { name: "Wireless Earbuds", price: 300, category: "Electronics" },
+    { name: "Study Chair", price: 180, category: "Furniture" },
+    { name: "Mechanical Keyboard", price: 220, category: "Electronics" }
+];
+
+
+// Callback Fn
+function isEligible(product) {
+
+    return (
+        product.category === "Electronics" &&
+        product.price > 200
+    );
+}
+
+
+// Main Fn
+function applyPromo(cart, promoCallback) {
+
+    let discountedCount = 0;
+    let totalSavings = 0;
+
+    // Transform cart
+    let updatedCart = cart.map(product => {
+
+        if (promoCallback(product)) {
+
+            let discount = product.price * 0.10;
+            discountedCount++;
+            totalSavings += discount;
+
+            return {
+                ...product,
+                price: product.price - discount,
+                isDiscounted: true
+            };
+        }
+
+        return {
+            ...product,
+            isDiscounted: false
+        };
+    });
+
+
+    // Final Summary
+    console.log("\nQ2: E-COMMERCE DISCOUNT APPLICATOR RESULT\n");
+    console.log(
+        `Promotion applied! ${discountedCount} products received discounts with a total savings of $${totalSavings.toFixed(2)}`
+    );
+
+
+    // Delayed Return
+    setTimeout(() => {
+
+        console.log("\n------UPDATED SHOPPING CART------");
+        console.log(updatedCart);
+
+    }, 1000);
+}
+// Fn Call
+applyPromo(cart, isEligible);
