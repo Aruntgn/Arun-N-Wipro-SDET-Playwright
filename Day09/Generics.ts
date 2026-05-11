f6f847d84a7619d7a967c2020ca940444d86bb60
Aruntgn <tgnarun@gmail.com>
2026-05-11 15:47:20 +0530

    Implementation of generic with constraints, Tuple, enum interface

--- Day09/Generics.ts
+++ Day09/Generics.ts
@@ -93,3 +93,16 @@ names.addItems("Gautham");
 names.addItems("Dharani");
 
 console.log(names.getItem(0), names.getItem(1), names.getItem(2), names.getItem(3)); // undefined for index 3
+
+interface HasLength {
+    length: number;
+}
+
+// generics with contraints | extends keyword
+function logLength<T extends HasLength>(arg: T): void { // T -> property .length --> string, arrays
+    console.log(arg.length);
+}
+
+logLength("Arun"); // string --> length
+logLength([1, 2, 3]); // array --> length
+// logLength(10); // number --> X
